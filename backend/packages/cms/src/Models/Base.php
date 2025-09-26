fdea59d32a3907cd3178b072a45913a5be422fd9
OskarP <oskar43619@wp.pl>
2025-09-26 19:16:11 +0200

    backend cms baisc mode impelnetations

---
+++ backend/packages/cms/src/Models/Base.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Op\Cms\Models;
+
+use Op\Cms\Traits\DefaultModel;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Abstract base model providing common logic for CMS models.
+ */
+abstract class Base extends Model {
+
+  use DefaultModel;
+
+  public function __construct(array $attributes = []) {
+    parent::__construct($attributes);
+    $this->appends = [...$this->defaultAppends, ...$this->appends ?? []];
+    // $this->fillable = [...$this->defaultFillable, ...$this->fillable ?? []];
+  }
+
+}
